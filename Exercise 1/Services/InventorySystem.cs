f1adb3243979f171444dcd395929fd732fc9b614
wm0709 <79418414+wm0709@users.noreply.github.com>
2021-03-01 19:18:29 -0600

    Exercise 1

---
+++ Exercise 1/Services/InventorySystem.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace HW3EX1B4.Services
+{
+    public class InventorySystem
+    {
+        public void Reserve(string sku, int quantity)
+        {
+            throw new InsufficientInventoryException();
+        }
+    }
+
+    public class InsufficientInventoryException : Exception
+    {
+    }
+}
---
+++ Exercise 1/Utility/Logger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW3EX1B4.Utility
+{
+    public static class Logger
+    {
+        public static void Error(string message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
